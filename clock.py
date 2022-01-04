c29fb42d145901c124a3eee2c8c21978478a47eb
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-04 10:02:39 +0530

    added funtions

--- clock.py
+++ clock.py
@@ -16,4 +16,35 @@ class main(Tkinter.Tk):
         return
 
     def create_background(self):
-        
+        self.image = Tkinter.PhotoImage(file='clock.gif')
+        self.canvas.create_image(150, 150, image=self.image)
+        return
+
+    def create_sticks(self):
+        self.sticks = []
+        for i in range(3):
+            store = self.canvas.creat_line(self.x, self.y, self.x+self.length,self.y+self.length,width=2, fill='red')
+            self.sticks.append(store)
+        return
+    
+    def update_class(self):
+        now = time.localtime()
+        t = time.strptime(str(now.tm_hour), "%H")
+        hour = int(time.strftime("%I", t))*5
+        now = (hour, now.tm_min, now.tm_sec)
+        # followling loop is for updating stick position
+        for n,i in enumerate(now):
+            x, y = self.canvas.coords(self.sticks[n])[0: 2]
+            cr = [x, y]
+            cr.append(self.length*math.cos(match.radians(i*6)-math.radians(90))+slef.x)
+            cr.append(self.length*math.sin(math.radians(i*6)-math.radians(90))+self.y)
+            self.canvas.coords(self.sticks[n], tuple(cr))
+        return
+
+if __name__ == '__main__':
+    root = main()
+    while True:
+        root.update()
+        root.update_idletasks()
+        root.update_class()
+    
